e3c5942f061d59ccf1dd79be20da7555502a3576
lavashik-mini <matiyukv41@gmail.com>
2025-01-29 21:09:44 +0300

    Added github action

---
+++ .github/README.md
@@ -0,0 +1,26 @@
+# PCapture-Lib is your best choise!
+PCapture-Lib is a powerful VScripts library that supercharges your Portal 2 modding capabilities. It provides advanced features like portal-aware ray tracing, optimized collision detection, enhanced data structures, and much more, simplifying complex scripting tasks and unlocking new gameplay possibilities.
+
+## Installation
+
+1. **Download:** Get the latest release of `PCapture-Lib.nut`.
+2. **Place:** Put the `PCapture-Lib.nut` file in your mod's `portal2/scripts/vscripts` directory. You can also place it in subdirectories within `vscripts` if you prefer (e.g., `portal2/scripts/vscripts/MyMod/PCapture-Lib.nut`).
+3. **Import:** Add the following line to your VScript file to import the library:
+
+    ```lua
+    IncludeScript("PCapture-Lib")
+    ```
+
+    *Note:* If you placed `PCapture-Lib.nut` in a subdirectory, adjust the import path accordingly. For example:
+
+    ```lua
+    IncludeScript("MyMod/PCapture-Lib")
+    ```
+
+That's it! You're now ready to use PCapture-Lib's features in your mod.
+
+## Get Started
+
+Check out the full documentation and examples in the [repository](https://github.com/IaVashik/PCapture-LIB/) to learn more about what PCapture-Lib can do.
+
+**Happy modding!**
---
+++ .github/create_all_in_one.py
@@ -0,0 +1,34 @@
+def create_all_in_one(input_file, output_file):
+    """
+    Creates an all-in-one version of PCapture-LIB by recursively
+    replacing IncludeScript calls with the contents of included files.
+
+    Args:
+        input_file (str): The path to the main PCapture-LIB file.
+        output_file (str): The path to the output file for the all-in-one version. 
+    """
+    with open(output_file, "w", encoding="utf-8") as outfile:
+        _process_file(input_file, outfile)
+
+def _process_file(file_path, outfile):
+    """
+    Recursively processes a file, replacing IncludeScript calls.
+
+    Args:
+        file_path (str): The path to the file to process.
+        outfile (file): The output file to write the processed code to.
+    """
+    file_path = file_path.replace("PCapture-LIB/", "")
+    with open(file_path, "r", encoding="utf-8") as infile:
+        for line in infile:
+            if line.startswith("IncludeScript("):
+                included_file_path = line[len("IncludeScript("):-2].replace('"', '').strip()
+                if not included_file_path.endswith(".nut"):
+                    included_file_path += ".nut"
+                _process_file(included_file_path, outfile)
+            else: 
+                outfile.write(line)
+        outfile.write("\n")
+    
+
+create_all_in_one("SRC/PCapture-Lib.nut", "OUTPUT/PCapture-Lib.nut") 
